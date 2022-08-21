cc0a1c661ac34153b8426e3f8107710e17ce0773
nataya1116 <https://github.com/nataya1116/nodeJs>
2022-08-22 00:21:40 +0900

    html 연결 중

--- app.js
+++ app.js
@@ -3,26 +3,30 @@ const { express, ejs, path } = require("./modules/common");
 const { sequelize } = require("./model");
 
 const userRouter = require("./routers/user_router");
+const indexRouter = require("./routers/index");
 
 const app = express();
 
 const PORT = 4000;
 
+// 뷰 폴더 내의 html을 views 절대 경로로 호출할 수 있게 처리
+app.set("views", path.join(__dirname, "view"));
 
-app.use(express.urlencoded({extended : false}));
+// html의 뷰 엔진을 ejs 랜더링 방식으로 바꾼다.
+app.engine("html", ejs.renderFile);
+
+// 뷰 엔진 설정을 html을 랜더링 할때 사용 하겠다.
+app.set("view engine", "html");
+
+app.use(express.urlencoded({ extended : false }));
 
 // public 폴더 내의 폴더 및 파일들의 경로를 절대경로로 호출할 수 있게 처리
 app.use(express.static('public'));
 
-// 뷰 폴더 내의 html을 views 절대 경로로 호출할 수 있게 처리
-app.set("views", path.join(__dirname, "view"));
-
 
 // 라우터 사용 설정
 app.use(userRouter);
-
-// html의 뷰 엔진을 ejs 랜더링 방식으로 바꾼다.
-app.engine("html", ejs.renderFile);
+app.use(indexRouter);
 
 app.listen(PORT, () => {
     console.log(PORT,"번 포트 대기 중");
--- routers/user_router.js
+++ routers/user_router.js
@@ -1,29 +1,9 @@
-const { express, fs, path, ejs } = require("../modules/common");
+const { express } = require("../modules/common");
 
 const router = express.Router();
 
-const app = express();
-
-// path.join함수는 매개변수를 받은 문자열들을 주소처럼 합쳐줌
-// path.join('a','b') = a/b 주소처럼 만들어줌
-// app2.js가 있는 위치 __dirname
-// views 폴더까지의 경로가 기본값 렌더링할 파일을 모아둔 폴더
-// app.set express에 값을 저장가능 밑에 구문은 views키에 주소값 넣은부분
-app.set("views", path.join(__dirname, "view"));
-
-app.engine("html", ejs.renderFile);
-
-app.set("view engine", "html");
-
 router.get('/login', (req, res) => {
-    fs.readFile("views/login", "utf-8", (err, page) => {
-        console.log(page);
-        res.send(page);
-    });
+    res.render("login");
 });
 
-router.get('/', (req, res) => {
-    res.send("ggg");
-});
-
-module.exports = router;
+module.exports = router;  
