38ecc00ed32344c67b38b089aedd96ade7fe4225
ceo7913 <ceo7913@naver.com>
2022-08-25 17:38:33 +0900

    qna table 만들었음

--- model/game_skin_products.js
+++ model/game_skin_products.js
@@ -1,40 +1,38 @@
 const Sequelize = require("sequelize");
 
 class GameSkinProducts extends Sequelize.Model {
-    static init(sequelize){
-        return super.init(
-            {
-                name : {
-                    type : Sequelize.STRING(50),
-                    allowNull : false,
-                    unique : true
-                },
-                imgUrl : {
-                    type : Sequelize.STRING,
-                    allowNull : false,
-                    unique : true
-                },
-                price : {
-                    type : Sequelize.INTEGER,
-                    allowNull : false,
-                }
-            },
-            {
-                sequelize,
-                underscored : true,
-                modelName : "GameSkinProducts",
-                tableName : "game_skin_products",
-                timestamps : true,
-                paranoid : true,
-                charset: "utf8",
-                collate: "utf8_general_ci"
-            }
-        )
-    }
+  static init(sequelize) {
+    return super.init(
+      {
+        name: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+          unique: true,
+        },
+        imgUrl: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        price: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        underscored: true,
+        modelName: "GameSkinProducts",
+        tableName: "game_skin_products",
+        timestamps: true,
+        paranoid: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
 
-    static associate(db) {
-        
-    }
+  static associate(db) {}
 }
 
 module.exports = GameSkinProducts;
--- model/index.js
+++ model/index.js
@@ -1,19 +1,20 @@
 const Sequelize = require("sequelize");
 
 const config = require("../config/config");
- 
+
 const User = require("./users");
 const Authority = require("./authority");
 const TipBoard = require("./tip_board");
 const TipReply = require("./tip_reply");
 const Chatting = require("./chatting");
 const GameSkinProducts = require("./game_skin_products");
+const QnaBoard = require("./qna_board");
 
 const sequelize = new Sequelize(
-    config.dev.database,
-    config.dev.username,
-    config.dev.password,
-    config.dev
+  config.dev.database,
+  config.dev.username,
+  config.dev.password,
+  config.dev
 );
 
 const db = {};
@@ -25,6 +26,7 @@ db.TipBoard = TipBoard;
 db.TipReply = TipReply;
 db.Chatting = Chatting;
 db.GameSkinProducts = GameSkinProducts;
+db.QnaBoard = QnaBoard;
 
 // 테이블 생성
 Authority.init(sequelize);
@@ -33,13 +35,15 @@ TipBoard.init(sequelize);
 TipReply.init(sequelize);
 Chatting.init(sequelize);
 GameSkinProducts.init(sequelize);
+QnaBoard.init(sequelize);
 
 // 테이블간의 관계 설정
-Authority.associate(db)
+Authority.associate(db);
 User.associate(db);
 TipBoard.associate(db);
 TipReply.associate(db);
 Chatting.associate(db);
 GameSkinProducts.associate(db);
+QnaBoard.associate(db);
 
 module.exports = db;
