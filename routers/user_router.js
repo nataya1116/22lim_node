c2466492168f1ba43421e6c2e747471ec8d64d2d
jinnyl22 <smile2374@gmail.com>
2022-08-25 20:11:03 +0900

    shop 레이아웃 완성

--- routers/user_router.js
+++ routers/user_router.js
@@ -4,15 +4,20 @@ const router = express.Router();
 
 const userController = require("../controllers/user_controller");
 
-router.get('/login2', (req, res) => {
-    res.render("login");
+router.get("/login2", (req, res) => {
+  res.render("login");
 });
 
-router.get('/login', (req, res) => {
-    res.render("login");
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// 임시로 만든 라우터! 헤더랑 푸터 붙임
+router.get("/shop", (req, res) => {
+  res.render("shop");
 });
 
 // router.post("/login", userController.loginTmp);
 router.post("/login", userController.login);
 
-module.exports = router;  
+module.exports = router;
