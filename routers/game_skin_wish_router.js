b28255bf8fc020c419e2b8247f4844b83916187a
nataya1116 <https://github.com/nataya1116/nodeJs>
2022-09-04 01:20:50 +0900

    세션 유효성 작업 중

--- routers/game_skin_wish_router.js
+++ routers/game_skin_wish_router.js
@@ -3,10 +3,10 @@ const { express } = require("../modules/common");
 const router = express.Router();
 
 const SkinWishController = require("../controllers/game_skin_wish_controller");
+const SessionMiddleware = require("../middlewares/session_middleware");
 
+router.post("/create", SessionMiddleware.validity, SkinWishController.create);
 
-router.post("/create", SkinWishController.create);
-
-router.post("/delete", SkinWishController.delete)
+router.post("/delete", SessionMiddleware.validity, SkinWishController.delete)
 
 module.exports = router;
