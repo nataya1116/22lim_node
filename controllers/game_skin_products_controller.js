c6402f68e532c3143c7ce977f9c1460597f66b50
nataya1116 <https://github.com/nataya1116/nodeJs>
2022-09-04 11:35:54 +0900

    링크 등 수정 중

--- controllers/game_skin_products_controller.js
+++ controllers/game_skin_products_controller.js
@@ -1,12 +1,14 @@
 const SkinProductsService = require("../service/game_skin_products_service");
+const TokenService = require("../service/token_service");
 
 module.exports.list = async (req, res) => {
     const accessToken = req.session?.access_token;
     const User = TokenService.verifyAccessToken(accessToken);
     const userId = User?.userId;
+    const point = User?.point;
 
     const result = await SkinProductsService.list(0, 1000);
     const list = result?.rows;
-
-    res.render("skin_list", { userId, list });
+    console.log(list);
+    res.render("skin_list", { userId, point, list });
 };
