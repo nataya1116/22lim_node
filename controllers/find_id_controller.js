57da73a300b2b64ee71b5bd4ec2ff11a6ba8cabf
ceo7913 <ceo7913@naver.com>
2022-09-05 10:48:57 +0900

    2

--- controllers/find_id_controller.js
+++ controllers/find_id_controller.js
@@ -3,53 +3,51 @@ const { mailer, jwt } = require("../modules/common");
 const { config } = require("../config/config");
 const TokenService = require("../service/token_service");
 
-
 module.exports.idEmailSend = (req, res) => {
-    let userEmail = req.body.userEmail;
-    const accessToken = req.session?.access_token;
-    const User = TokenService.verifyAccessToken(accessToken);
-    const userId = User?.userId;
+  let userEmail = req.body.userEmail;
+  const accessToken = req.session?.access_token;
+  const User = TokenService.verifyAccessToken(accessToken);
+  const userId = User?.userId;
 
-    UserService.findId(userEmail).then((e) => {
-      if (e !== null) {
-        let sendmail = {
-          toEmail: userEmail,
-          subject: `안녕하세요 22lim 아이디 조회 결과입니다.`,
-          text: `${userEmail}님의 아이디는, <h1>${userId}</h1> 입니다.`,
-        };
-  
-        let transpoter = mailer.createTransport({
-          service: "Naver",
-          port: 587, // 25 587
-          host: "smtp.naver.com",
-          auth: {
-            user: config.mailer.user,
-            pass: config.mailer.pw,
-          },
-        });
-  
-        let mailoption = {
-          from: config.mailer.user,
-          to: sendmail.toEmail,
-          subject: sendmail.subject,
-          html: sendmail.text,
-        };
-  
-        transpoter.sendMail(mailoption, (err, info) => {
-          if (err) {
-            console.log(err); 
-            res.send("err");
-          }
-          else console.log("send success", info.response);
-        });
-        res.send("suc");
-      } else {
-        res.send("fail");
-      }
-    });
-  };
-  module.exports.findIdView = (req, res) => {
-    const accessToken = req.session?.access_token;
-    const User = TokenService.verifyAccessToken(accessToken);
-    res.render("find_id", { User });
-  };
+  UserService.findId(userEmail).then((e) => {
+    if (e !== null) {
+      let sendmail = {
+        toEmail: userEmail,
+        subject: `안녕하세요 22lim 아이디 조회 결과입니다.`,
+        text: `${userEmail}님의 아이디는, <h1>${e.userId}</h1> 입니다.`,
+      };
+
+      let transpoter = mailer.createTransport({
+        service: "Naver",
+        port: 587, // 25 587
+        host: "smtp.naver.com",
+        auth: {
+          user: config.mailer.user,
+          pass: config.mailer.pw,
+        },
+      });
+
+      let mailoption = {
+        from: config.mailer.user,
+        to: sendmail.toEmail,
+        subject: sendmail.subject,
+        html: sendmail.text,
+      };
+
+      transpoter.sendMail(mailoption, (err, info) => {
+        if (err) {
+          console.log(err);
+          res.send("err");
+        } else console.log("send success", info.response);
+      });
+      res.send("suc");
+    } else {
+      res.send("fail");
+    }
+  });
+};
+module.exports.findIdView = (req, res) => {
+  const accessToken = req.session?.access_token;
+  const User = TokenService.verifyAccessToken(accessToken);
+  res.render("find_id", { User });
+};
